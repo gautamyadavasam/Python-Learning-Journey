60ef6f62118d5cbbedf28cc1d828fc4167666d23
Gautam Yadav <gautamyadav@Gautams-MacBook-Air.local>
2026-06-04 10:05:40 +0530

    Os libraya learning basic

---
+++ python Edu py  vs code/loops_&_Conditions.py
@@ -0,0 +1,5 @@
+import urllib.request
+urllib.request.urlretrieve(
+    'https://hub.jovian.ml/wp-content/uploads/2020/08/climate.csv',
+    'climate.txt'
+)
---
+++ python_learning_collab/prectice.py
@@ -0,0 +1,8 @@
+
+i = 20
+
+while 0 <= i:
+  i -=1
+  if i % 6 ==0:
+    continue
+  print(i)
